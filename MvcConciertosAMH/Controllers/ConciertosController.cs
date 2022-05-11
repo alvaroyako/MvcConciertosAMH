08f6cdc40094a38075b5ffd2b870a7d51dade1a6
alvaroyako <alvaroyako2000@hotmail.com>
2022-05-11 12:49:43 +0200

    Add project files.

---
+++ MvcConciertosAMH/Controllers/ConciertosController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcConciertosAMH.Models;
+using MvcConciertosAMH.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcConciertosAMH.Controllers
+{
+    public class ConciertosController : Controller
+    {
+        private ServiceApiConciertos service;
+
+        public ConciertosController(ServiceApiConciertos service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Evento> eventos = await this.service.GetEventosAsync();
+            return View(eventos);
+        }
+
+        public async Task<IActionResult> Categorias()
+        {
+            List<Categoria> categorias = await this.service.GetCategoriasAsync();
+            return View(categorias);
+        }
+
+        public async Task<IActionResult> EventosPorCategoria(int idcategoria)
+        {
+            List<Evento> eventos = await this.service.GetEventosCategoriaAsync(idcategoria);
+            return View(eventos);
+        }
+
+        public async Task <IActionResult> CrearEvento()
+        {
+            ViewData["CATEGORIAS"] = await this.service.GetCategoriasAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> CrearEvento(string nombre, string artista,int idcategoria)
+        {
+            Evento e = new Evento();
+            e.IdEvento = 0;
+            e.Nombre = nombre;
+            e.Artista = artista;
+            e.IdCategoria = idcategoria;
+
+            await this.service.InsertEventoAsync(e);
+            return RedirectToAction("Index");
+        }
+
+        public async Task <IActionResult> Delete(int idevento)
+        {
+            await this.service.DeleteEventoAsync(idevento);
+            return RedirectToAction("Index");
+        }
+
+        public async Task <IActionResult> CambiarCategoria(int idevento)
+        {
+            ViewData["CATEGORIAS"] = await this.service.GetCategoriasAsync();
+            Evento evento = await this.service.FindEventoAsync(idevento);
+            return View(evento);
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> CambiarCategoria(int idevento,string nombre,string artista,int idcategoria)
+        {
+            Evento evento = new Evento();
+            evento.IdEvento = idevento;
+            evento.Nombre = nombre;
+            evento.Artista = artista;
+            evento.IdCategoria = idcategoria;
+
+            await this.service.CambiarCategoriaAsync(evento);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcConciertosAMH/Services/ServiceApiConciertos.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Extensions.Configuration;
+using MvcConciertosAMH.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcConciertosAMH.Services
+{
+    public class ServiceApiConciertos
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceApiConciertos(IConfiguration configuration)
+        {
+            this.UrlApi = configuration.GetValue<string>("ApiUrls:ApiConciertosAWS");
+            this.Header =
+                new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T> CallApi<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                string url = this.UrlApi + request;
+                HttpResponseMessage response =
+                    await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data =
+                        await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        //api/categorias
+        public async Task<List<Categoria>> GetCategoriasAsync()
+        {
+            string request = "/api/categorias";
+            List<Categoria> categorias =
+                await this.CallApi<List<Categoria>>(request);
+            return categorias;
+        }
+
+        //api/eventos
+        public async Task<List<Evento>> GetEventosAsync()
+        {
+            string request = "/api/eventos";
+            List<Evento> eventos =
+                await this.CallApi<List<Evento>>(request);
+            return eventos;
+        }
+
+        //api/eventos/FindEvento/idevento
+        public async Task<Evento> FindEventoAsync(int idevento)
+        {
+            string request = "/api/eventos/findevento/"+idevento;
+            Evento evento =
+                await this.CallApi<Evento>(request);
+            return evento;
+        }
+
+        //api/eventos/geteventoscategoria/idcategoria
+        public async Task<List<Evento>> GetEventosCategoriaAsync(int idcategoria)
+        {
+            string request = "/api/eventos/geteventoscategoria/"+idcategoria;
+            List<Evento> eventos =
+                await this.CallApi<List<Evento>>(request);
+            return eventos;
+        }
+
+        //api/eventos
+        public async Task InsertEventoAsync(Evento evento)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/eventos";
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+
+                string json = JsonConvert.SerializeObject(evento);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage reponse = await client.PostAsync(this.UrlApi+request, content);
+            }
+        }
+
+        //api/eventos/delete/idevento
+        public async Task DeleteEventoAsync(int idevento)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/eventos/delete/" + idevento;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.DeleteAsync(this.UrlApi+request);
+            }
+        }
+
+        //api/eventos
+        public async Task CambiarCategoriaAsync(Evento evento)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/eventos";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                Evento e = new Evento();
+                e.IdEvento = evento.IdEvento;
+                e.Nombre = evento.Nombre;
+                e.Artista = evento.Artista;
+                e.IdCategoria = evento.IdCategoria;
+
+                string json = JsonConvert.SerializeObject(e);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(this.UrlApi+request, content);
+            }
+        }
+
+    }
+}
